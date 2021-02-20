b23002f5ac5faa0005c76d39bf02ca286c93a9fe
brackenBYUI <don18004@byui.edu>
2021-02-20 14:34:57 -0700

    Update file structure

--- lesson4/js/scripts.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("hide");
-}
-
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('today-date').textContent = new Date().toLocaleDateString('en-US', options);
