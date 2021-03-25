691339bc8d380878a6ffbfe5208c61341715f097
brackenBYUI <don18004@byui.edu>
2021-03-24 19:12:41 -0600

    Lesson 13

---
+++ lesson13/js/scripts.js
@@ -0,0 +1,2 @@
+const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('last-update').textContent = new Date(document.lastModified).toLocaleDateString('en-US', options);
