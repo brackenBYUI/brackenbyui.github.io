6fd5e6fe5099e7265cac062e00fb3e181e56e51a
brackenBYUI <don18004@byui.edu>
2021-04-06 16:08:14 -0600

    Finish header

--- lesson13/js/scripts.js
+++ lesson13/js/scripts.js
@@ -1,6 +1,9 @@
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
+    document.getElementById("hamburger-menu").classList.toggle("hide");
+    document.getElementById("primaryNav").classList.toggle("mobile-menu");
 }
+
 
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById('last-update').textContent = new Date(document.lastModified).toLocaleDateString('en-US', options);
