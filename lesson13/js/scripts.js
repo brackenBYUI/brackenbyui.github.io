34ecb81edf45f3c7afbc98eebbd08313d06fbd32
brackenBYUI <don18004@byui.edu>
2021-04-07 20:59:05 -0600

    Adding list and grid views

--- lesson13/js/scripts.js
+++ lesson13/js/scripts.js
@@ -4,6 +4,15 @@ function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("mobile-menu");
 }
 
+function listView() {
+    document.querySelector(".business").classList.add("list-view");
+}
+
+function gridView() {
+    document.querySelector(".business").classList.remove("list-view");
+
+}
+
 
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById('last-update').textContent = new Date(document.lastModified).toLocaleDateString('en-US', options);
